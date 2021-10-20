dcd2b1aca75ef1ff493445135878537d1dca403c
Андрій Горденко <andreygordenko3002@gmail.com>
2021-10-20 20:26:58 +0300

    added tobi

---
+++ lohyetomy/lohyetomy.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	int a = 0;
+
+	cout << "Enter number: ";
+	cin >> num;
+	cout << endl;
+
+	while (a < num)
+	{
+		cout << a << ", ";
+		a++;
+	}
+	cout << a++<< "." << endl;
+}
