9dbee7737f1effcabf7bf05229da829bfbab86fb
Андрій Горденко <andreygordenko3002@gmail.com>
2021-10-20 05:29:45 +0300

    lohy

---
+++ zad/zad.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int star = 0;
+
+	cout << "Enter please number: ";
+	cin >> star;
+	cout << endl;
+
+	for (int i = 0; i < star; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+}
